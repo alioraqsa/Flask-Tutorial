9be3d55b1dbd98dd56c24a19d9a65e92e764d1a1
Ali Aqsa <AliorAqsa@MacBook-Pro.local>
2022-11-25 02:20:37 +0800

    Setup (base, signup) template, add flash to signup

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<h1>Login</h1>"
+    return render_template("login.html")
 
 
 @auth.route('/logout')
 def logout():
-    return "<h1>Logout</h1>"
+    return render_template("logout.html")
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<h1>Sign Up</h1>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len('password1') < 6:
+            flasg('Password must be at least 6 characters.', category='error')
+    return render_template("sign_up.html")
